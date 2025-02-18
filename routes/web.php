f262c3a8e52b3347f6834fbf773d7d08a5b5ad28
= <belqisivana7@gmail.com>
2025-02-18 14:20:08 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+ 
+Route::get('/SelamatDatang', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Nama: Belqis Ivana Fidelia Arfany <br> Nim: 2341760075';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+    
+Route::get('/articles/{id}', function ($articlesId) {
+    return 'id ke-'.$articlesId;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
+    
+    
